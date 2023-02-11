5b623f7dcac5ee0c7874e5212a0bfd6f78694e32
LetsCodeManh <manh.ng@icloud.com>
2023-02-11 01:04:46 +0100

    feat: HeroSection styling

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import "./styles.css";
+import theFuck from "../../assets/TheFuck.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
-function Hero() {
+const Hero: React.FC = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section id="hero" className="hero">
+      <div className="hero__left__side">
+        <h1 className="section__header">
+          Die schnelle & einfache Grünstrom-Lösung für Ihr Unternehmen
+        </h1>
+        <a href="#survey" className="hero__button">
+          <FontAwesomeIcon icon={faArrowRight} /> zur Lösung
+        </a>
+      </div>
+      <div className="hero__right__side">
+        <img src={theFuck} alt="something" className="hero__image"/>
+      </div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
--- src/components/Survey/Survey.tsx
+++ src/components/Survey/Survey.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import "./styles.css"
 
 function Survey() {
-  return <div>Survey</div>;
+  return <div className="survey">Survey</div>;
 }
 
 export default Survey;
