aebb14faebbec2f37acffe7454fa930c32a24fc8
LetsCodeManh <manh.ng@icloud.com>
2023-02-12 21:43:45 +0100

    feat: adding Impressum

--- src/components/Footer/Impressum.tsx
+++ src/components/Footer/Impressum.tsx
@@ -1,8 +1,47 @@
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
-import { RouteProps } from "react-router-dom";
+import { Link, RouteProps } from "react-router-dom";
 
 const Impressum: React.FC<RouteProps> = () => {
-  return <h1>Impressum</h1>;
+  return (
+    <main className="datenschutz">
+      <Link to="/energyspektrum/" className="back__button">
+        <FontAwesomeIcon icon={faArrowLeft} /> Back
+      </Link>
+      <h1>Impressum</h1>
+      <section>
+        <div>
+          <h3>Angaben gemäß § 5 TMG</h3>
+          <p>Oskar Agovic, Energiespektrum, Unterm Hagen 11, 37079 Göttingen</p>
+        </div>
+        <div>
+          <h3>Kontakt</h3>
+          <p>
+            Telefon: 017673882007 E-Mail: <br />
+            info@energiespektrum.com
+          </p>
+        </div>
+        <div>
+          <h3>EU-Streitschlichtung</h3>
+          <p>
+            Die Europäische Kommission stellt eine Plattform zur
+            Online-Streitbeilegung (OS) bereit:
+            https://ec.europa.eu/consumers/odr/. Unsere E-Mail-Adresse finden
+            Sie oben im Impressum.
+          </p>
+        </div>
+        <div>
+          <h3>Verbraucherstreitbeilegung/Universalschlichtungsstelle</h3>
+          <p>
+            Wir sind nicht bereit oder verpflichtet, an
+            Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle
+            teilzunehmen.
+          </p>
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default Impressum;
